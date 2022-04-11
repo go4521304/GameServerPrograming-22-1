0abecb05fec6f2a946298bfc146f13de6dd01a17
Kclient <go4521304@gmail.com>
2022-04-11 19:09:11 +0900

    멀티스레드 프로그램 최적회

--- MultiThreadProgramming/thread.cpp
+++ MultiThreadProgramming/thread.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <chrono>
+#include <mutex>
 using namespace std;
+using namespace std::chrono;
 
-constexpr int NUM_THREADS = 10;
+constexpr int NUM_THREADS = 4;
+constexpr int LOOP = 500000000;
 
-void thread_func(int th_id)
+volatile int sum = 0;
+mutex my_lock;
+
+void thread_func(int num_threads)
 {
-	cout << "I am a Thread " << th_id << ".\n";
+	volatile int local_sum = 0;
+	for (int i = 0; i < LOOP / num_threads; ++i)
+	{
+		local_sum = local_sum + 2;
+	}
+	my_lock.lock();
+	sum = sum + local_sum;
+	my_lock.unlock();
 }
+
 
 int main()
 {
-	vector<thread> threads;
-	for (int i = 0; i < NUM_THREADS; ++i)
-		threads.emplace_back(thread_func, i);
-
-	cout << "Hello World from main\n";
-
-	for (auto& th : threads)
-		th.join();
-
+	for (int num = 1; num <= 16; num *= 2)
+	{
+		sum = 0;
+		vector<thread> threads;
+		auto start_t = high_resolution_clock::now();
+		for (int i = 0; i < num; ++i)
+			threads.emplace_back(thread_func, num);
+		for (auto& th : threads)
+			th.join();
+		auto end_t = high_resolution_clock::now();
+		auto exec_t = end_t - start_t;
+		cout << num << " Threads, ";
+		cout << "Sum = " << sum << " Multi Thread Exec Time = ";
+		cout << duration_cast<milliseconds>(exec_t).count() << "ms.\n";
+	}
 }
