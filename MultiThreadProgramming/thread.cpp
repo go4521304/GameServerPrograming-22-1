4f70803a7c5d6fbb8974dc6e8420db1edbead503
User <User@DESKTOP-MOCJD6J>
2022-04-12 13:54:11 +0900

    배열에 더해서 합산

--- MultiThreadProgramming/thread.cpp
+++ MultiThreadProgramming/thread.cpp
@@ -9,18 +9,20 @@ using namespace std::chrono;
 constexpr int NUM_THREADS = 4;
 constexpr int LOOP = 500000000;
 
-volatile int sum = 0;
+volatile int sum;
+volatile int t_sum[16];
 mutex my_lock;
 
-void thread_func(int num_threads)
+void thread_func(int th_id, int num_threads)
 {
 	volatile int local_sum = 0;
 	for (int i = 0; i < LOOP / num_threads; ++i)
 	{
-		local_sum = local_sum + 2;
+		t_sum[th_id] = t_sum[th_id] + 2;
+		//local_sum = local_sum + 2;
 	}
 	my_lock.lock();
-	sum = sum + local_sum;
+	sum = sum + t_sum[th_id];
 	my_lock.unlock();
 }
 
@@ -30,10 +32,11 @@ int main()
 	for (int num = 1; num <= 16; num *= 2)
 	{
 		sum = 0;
+		for (auto& s : t_sum) s = 0;
 		vector<thread> threads;
 		auto start_t = high_resolution_clock::now();
 		for (int i = 0; i < num; ++i)
-			threads.emplace_back(thread_func, num);
+			threads.emplace_back(thread_func, i, num);
 		for (auto& th : threads)
 			th.join();
 		auto end_t = high_resolution_clock::now();
